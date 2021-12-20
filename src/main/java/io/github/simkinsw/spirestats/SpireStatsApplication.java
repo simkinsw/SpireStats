4869645d412194fb6f598e0b18f4739f34f3b131
Will <wsimkins2013@gmail.com>
2021-12-19 21:46:32 -0500

    setup

---
+++ src/main/java/io/github/simkinsw/spirestats/SpireStatsApplication.java
@@ -0,0 +1,13 @@
+package io.github.simkinsw.spirestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpireStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpireStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/simkinsw/spirestats/SpireStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.simkinsw.spirestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpireStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
